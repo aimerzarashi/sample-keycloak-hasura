a93cbe766fefb3ee5532e1d91168ddf3da47a574
aimerzarashi <git.aimerzarashi.com>
2024-05-24 22:13:12 +0900

    updated model

--- web/prisma/migrations/20240524131246_/migration.sql
+++ web/prisma/migrations/20240524131246_/migration.sql
@@ -1,13 +1,8 @@
 -- CreateTable
 CREATE TABLE "IndividualNumber" (
     "id" TEXT NOT NULL DEFAULT gen_random_uuid(),
-    "firstName" TEXT NOT NULL,
-    "lastName" TEXT NOT NULL,
-    "birthday" TIMESTAMP(3) NOT NULL,
-    "gender" TEXT NOT NULL,
-    "adderess" TEXT NOT NULL,
+    "userId" TEXT NOT NULL,
     "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
-    "updatedAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
 
     CONSTRAINT "IndividualNumber_pkey" PRIMARY KEY ("id")
 );
